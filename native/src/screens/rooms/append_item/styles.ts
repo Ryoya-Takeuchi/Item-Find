798477368f6a86e28fab3eccd1b9082619d357a3
Ryoya-Takeuchi <trho.20@icloud.com>
2021-01-23 17:25:02 +0900

    add append_item

---
+++ native/src/screens/rooms/append_item/styles.ts
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {StyleSheet} from 'react-native';
+
+
+const styles =  StyleSheet.create({
+	wrap_area : {
+		flex : 1,
+	},
+	photo_area : {
+		flex : 1,
+		backgroundColor : "#E2E2E2",
+		flexDirection : "row"
+	},
+	form_area : {
+		flex : 5,
+		backgroundColor : "#ffffff"
+	},
+})
+
+export default styles;
