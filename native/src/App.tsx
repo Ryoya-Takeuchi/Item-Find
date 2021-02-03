0c09c26c46b8c7732a0a3989cdf4ed4e5b91b1be
Ryoya-Takeuchi <trho.20@icloud.com>
2021-02-04 00:15:18 +0900

    fix app

--- native/src/App.tsx
+++ native/src/App.tsx
@@ -10,17 +10,22 @@ import React from 'react';
 import { Root } from 'native-base';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppContainer from './navigations/index';
+import AuthProvider from './contexts/Auth';
+import ModuleProvider from './contexts/Module/index';
 
 const App =  () => {
-
+ 
 	return (
 		<Root>
 			<SafeAreaProvider>
-				<AppContainer/>
+				<AuthProvider>
+					<ModuleProvider>
+						<AppContainer/>
+					</ModuleProvider>
+				</AuthProvider>
 			</SafeAreaProvider>
 		</Root>
 	);
 };
-
 
 export default App;
