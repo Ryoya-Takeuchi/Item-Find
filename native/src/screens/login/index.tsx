3cba330d4bc0c695afa3c8465f7e9204833362a6
Ryoya-Takeuchi <trho.20@icloud.com>
2021-02-07 19:36:59 +0900

    add login

--- native/src/screens/login/index.tsx
+++ native/src/screens/login/index.tsx
@@ -8,55 +8,104 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
   View,
   Dimensions,
   TouchableHighlight,
   Image,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  TextInput
 } from 'react-native';
 
 import { Container, Header, Content, Form, Item, Input, Label, Button,Text, Icon } from 'native-base';
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { FloatingLabelInput } from 'react-native-floating-label-input';
 import { useNavigation } from 'react-navigation-hooks';
 import * as Screens from '../../navigations/Screens';
+import auth from '@react-native-firebase/auth';
 
 const window = Dimensions.get("window");
 
 const LoginScreen = () => {
-
+	const [email , setEmail] = React.useState<string>("trho.26@sample.com");
+	const [password , setPassword] = React.useState<string>("password");
+	const [rotate , setRotate] = React.useState<number>(-80);
 	const {navigate} = useNavigation();
 
+	React.useEffect(() => {
+		setInterval(() => {setRotate(rotate => rotate + 10)},5000)
+	},[])
+
 	const login = () => {
-		navigate({routeName : 'AuthenticatedNavigation'})
+		if(email == "") {
+			return;
+		}
+		if(password == "") {
+			return;
+		}
+		auth().signInWithEmailAndPassword(email,password)
+		.then((user) => {
+			console.log(user);
+		}).catch((error) => {
+			var errorCode = error.code;
+			var errorMessage = error.message;
+		})
 	}
+
 
   return (
     <>
 		{/* <AppContainer /> */}
 		<KeyboardAvoidingView style={styles.body} behavior="padding">
 			<View style={styles.top}>
-				<View style={{flexDirection : "row" , alignItems : "center"}}>
-					<Image resizeMode='contain' source={require('../../images/enpitsu.png')} style={{transform: [{ rotate: "180deg" }] , width : window.width * 0.05 , height : window.width * 0.15}} />
+				<View style={{flexDirection : "row" , alignItems : "center" , justifyContent : "flex-end"}}>
+					<View style={{flexDirection : "column"}}>
+						<Image resizeMode='contain' source={require('../../resources/keshigomu.png')} style={{transform: [{ rotate: `${rotate}deg` }] , width : window.width * 0.05 , height : window.width * 0.03}} />
+						<Image resizeMode='contain' source={require('../../resources/enpitsu.png')} style={{transform: [{ rotate: "180deg" }] , width : window.width * 0.05 , height : window.width * 0.1}} />
+					</View>
 					<Text style={{fontSize: RFValue(window.width * 0.1) ,  color : '#ffffff' , fontFamily: 'American Typewriter' , fontWeight: 'bold'}}>tem_Find</Text>
 				</View>
 				<Icon name='archive' type="FontAwesome" style={{fontSize: window.width * 0.3, color: '#ffffff'}}/>
 			</View>
 			<View style={styles.bottom}>
-				<Form style={styles.from_area}>
-					<Item floatingLabel>
-						<Label>Username</Label>
-						<Input />
-						</Item>
-						<Item floatingLabel last>
-						<Label>Password</Label>
-						<Input />
-					</Item>
+				{/* <Form style={styles.from_area}> */}
+					{/* <Item floatingLabel> */}
+						{/* <Label>email</Label> */}
+						<TextInput
+							onChangeText={(text) => setEmail(text)}
+							value={email}/>
+						<FloatingLabelInput
+							label={'email'}
+							// isPassword
+							// togglePassword={show}
+							value={email}
+							onChangeText={(value) => setEmail(value)}
+							// customShowPasswordComponent={<Text>Show</Text>}
+							// customHidePasswordComponent={<Text>Hide</Text>}
+						/>
+						<FloatingLabelInput
+							label={'password'}
+							isPassword
+							// togglePassword={show}
+							value={email}
+							onChangeText={(value) => setPassword(value)}
+							// customShowPasswordComponent={<Text>Show</Text>}
+							// customHidePasswordComponent={<Text>Hide</Text>}
+						/>
+							{/* <Input/> */}
+						{/* </Item> */}
+						{/* <Item floatingLabel last> */}
+						{/* <Label>Password</Label> */}
+						<TextInput
+							onChangeText={(text) => setPassword(text)}
+							value={password}
+							defaultValue={""}/>
+							{/* <Input/> */}
+					{/* </Item> */}
 					<Button rounded full style={styles.login_but} onPress={login}>
 						<Text style={{color : "#ffffff"}}>ログインして始める</Text>
 					</Button>
-				</Form>
+				{/* </Form> */}
 				<View style={styles.new_area}>
 					<Text>新規登録されていない場合は</Text>
 					<TouchableHighlight onPress={() => alert("新規登録！！")}>
