c1b4850b6b4fe1e66c96e5402a65220c2066c51f
Ryoya-Takeuchi <trho.20@icloud.com>
2021-02-07 19:37:17 +0900

    fix setting

--- native/src/screens/rooms/setting/index.tsx
+++ native/src/screens/rooms/setting/index.tsx
@@ -1,11 +1,34 @@
 import * as React from 'react';
-import { View , Text , SafeAreaView} from 'react-native';
+import { Text , SafeAreaView} from 'react-native';
+import {
+	Content,
+	List,
+	ListItem,
+	Button
+} from 'native-base';
+import {AuthContext} from '../../../contexts/Auth'
+import {styles} from './styles'; 
+
 
 export default () => {
+	const {singnout , currentUser} = React.useContext(AuthContext)
 
 	return (
-		<SafeAreaView>
-			<Text>setting_room</Text>
-		</SafeAreaView>
+		<List>
+			<ListItem itemDivider style={styles.itemDivider}>
+				<Text>ユーザ情報</Text>
+			</ListItem>
+			<ListItem>
+				<Text>{currentUser}</Text>
+			</ListItem>
+			<ListItem itemDivider style={styles.itemDivider}>
+				<Text>サインアウト</Text>
+			</ListItem>
+			<ListItem>
+				<Button onPress={singnout} transparent>
+					<Text style={styles.singnout_button}>サインアウト</Text>
+				</Button>
+			</ListItem>
+		</List>
 	);
 }
---
+++ native/src/screens/rooms/setting/styles.ts
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {StyleSheet} from 'react-native';
+
+
+export const styles = StyleSheet.create({
+	itemDivider : {
+		backgroundColor : "#cccccc"
+	},
+	singnout_button : {
+		color : "#E88D8D",
+		fontWeight : '900',
+	}
+	
+})
