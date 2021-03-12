e6bf2ae9b012543ddd5e872fdcf39d9e825ce244
Ryoya-Takeuchi <trho.20@icloud.com>
2021-03-13 01:16:29 +0900

    add #13

---
+++ native/src/modules/hooks/useRooms.ts
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import firestore from '@react-native-firebase/firestore';
+import {IHierarchy} from '../models/entities/Hierarchy';
+import {IRoom} from '../models/entities/Room';
+
+interface IHook {
+	getAllRoom : () => void,
+	roomList : IFormTemp[]
+}
+
+export interface IFamily {
+	avatar_uri : string,
+	email : string,
+	family_id : string,
+	family_name : string,
+	given_name : string,
+	id : string,
+	nick_name : string
+};
+
+interface IFormTemp {
+	label : string,
+	value : string
+}
+
+export default function useRooms(familyCode : string) : IHook {
+	const [roomList , setRoomList] = React.useState<IFormTemp[]>([]);
+
+	const getAllRoom = React.useCallback(() => {
+		async function process() {
+			const ref = await firestore().collection('home').doc(familyCode).collection('hierarchys');
+			const snapshot = await ref.get();
+			const hierarchys = await Promise.all(
+				(snapshot).docs.map((value) => value.data() as IHierarchy )
+			)
+			const rooms = await Promise.all(
+				hierarchys.map(async(hierarchy) => {
+					const roomSnapshot =  await ref.doc(hierarchy.id).collection('rooms').get();
+					return (roomSnapshot).docs.map((doc) => {
+						const room = doc.data() as IRoom;
+						return {label : room.room_name,value: room.id}
+					})
+				})
+			);
+			return rooms.reduce((acc, val) => acc.concat(val), []);
+		};
+
+		process()
+		.then((result) => {
+			setRoomList(result);
+		})
+	},[roomList])
+
+	return{
+		getAllRoom,
+		roomList
+	}
+}
