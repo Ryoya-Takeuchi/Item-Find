de5a4eeb642f815d65e14fb37b211c7a4e6aa586
Ryoya-Takeuchi <trho.20@icloud.com>
2021-06-01 23:23:54 +0900

    fix #17

--- native/src/modules/hooks/useCloudRegister.ts
+++ native/src/modules/hooks/useCloudRegister.ts
@@ -1,42 +1,110 @@
-import * as React from 'react';
-import firestore,{FirebaseFirestoreTypes} from '@react-native-firebase/firestore';
-import {IItem} from '../../modules/models/entities/Item'
+import * as React from "react";
+import firestore from "@react-native-firebase/firestore";
+import { IItem } from "../../modules/models/entities/Item";
+import { TFormRoomProps } from "../hooks/useValidation";
+import { v4 as uuidv4 } from "uuid";
 
-type State = 'start' | 'done' | 'error' | 'init';
+type State = "start" | "done" | "error" | "init";
 interface ICloudRegisterHook {
-
+  itemRegister: (
+    hierarchyId: string,
+    item: IItem
+  ) => Promise<{ itemId: string } | Error>;
+  roomRegister: (
+    hierarchyId: string,
+    room: TFormRoomProps
+  ) => Promise<{ roomId: string } | Error>;
 }
-export default function useCloudRegister(familyCode : string) {
-	const [state , setState] = React.useState<State>('init');
 
-	const itemRegister = React.useCallback(async(hierarchyId : string,item : IItem) => {
-		try {
-			const res = await firestore()
-			.collection('test')
-			.doc(familyCode)
-			.collection('hierarchys')
-			.doc(hierarchyId)
-			.collection('items')
-			.add({
-				...item
-			})
-	
-			await firestore()
-			.doc(`home/${familyCode}/hierarchys/${hierarchyId}/items/${res.id}`)
-			.update({
-				...item, id : res.id
-			})
-			return {itemID : res.id}
-		} catch (error) {
-			console.log(error.message)
-			return new Error('登録処理でエラーが発生しました。')
-		}
-		
-	},[state,setState]);
+const cloudRegister = async (
+  type: "items" | "rooms",
+  familyCode: string,
+  hierarchyId: string,
+  docId: string,
+  createData: any
+) => {
+  await firestore()
+    .collection("home")
+    .doc(familyCode)
+    .collection("hierarchys")
+    .doc(hierarchyId)
+    .collection(type)
+    .doc(docId)
+    .set({ ...createData });
+};
+const cloudDataUpdate = async (
+  type: "items" | "rooms",
+  familyCode: string,
+  hierarchyId: string,
+  rootId: string,
+  updateData: any
+) => {
+  await firestore()
+    .doc(`home/${familyCode}/hierarchys/${hierarchyId}/${type}/${rootId}`)
+    .set({ ...updateData }, { merge: true });
+};
+export default function useCloudRegister(
+  familyCode: string
+): ICloudRegisterHook {
+  const [state, setState] = React.useState<State>("init");
 
+  const itemRegister = React.useCallback(
+    async (hierarchyId: string, item: IItem) => {
+      try {
+        //storeでの管理番号
+        const uuid = uuidv4();
+        await cloudRegister("items", familyCode, hierarchyId, uuid, item);
+        return { itemId: uuid };
+      } catch (error) {
+        return new Error("登録処理でエラーが発生しました。");
+      }
+    },
+    [state, setState]
+  );
 
+  const roomRegister = React.useCallback(
+    async (hierarchyId: string, inputRoom: TFormRoomProps) => {
+      // firestoreに登録する形に成形
+      const { room_name, hierarchy_id, parentRoom_id, private_ids } = inputRoom;
+      //storeでの管理番号
+      const uuid = uuidv4();
+      try {
+        const roomData = {
+          id: uuid,
+          room_name,
+          private_ids,
+          is_root: parentRoom_id != "" ? false : true,
+          room_ids: [],
+          order_by: 10,
+          create_at: firestore.Timestamp.fromDate(new Date()),
+          update_at: firestore.Timestamp.fromDate(new Date()),
+        };
+        // fireStoreに新規登録
+        await cloudRegister("rooms", familyCode, hierarchyId, uuid, roomData);
 
-	return {
-		itemRegister
-	}
+        if (parentRoom_id != "") {
+          // 親ルームに子ルームを追加
+          await cloudDataUpdate(
+            "rooms",
+            familyCode,
+            hierarchyId,
+            parentRoom_id,
+            {
+              room_ids: [uuid],
+            }
+          );
+        }
+
+        return { roomId: uuid };
+      } catch (error) {
+        return new Error("登録処理でエラーが発生しました。");
+      }
+    },
+    [state, setState]
+  );
+
+  return {
+    itemRegister,
+    roomRegister,
+  };
 }
