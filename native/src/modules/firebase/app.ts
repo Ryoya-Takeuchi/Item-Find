ce6dd8bf463961cd94333b08c5c3c782ea42757b
Ryoya-Takeuchi <trho.20@icloud.com>
2021-06-01 23:34:53 +0900

    fix firebase/app

--- native/src/modules/firebase/app.ts
+++ native/src/modules/firebase/app.ts
@@ -1,32 +1,32 @@
-import firebase from '@react-native-firebase/app';
-import firestore from '@react-native-firebase/firestore';
-import '@react-native-firebase/auth';
+import firebase from "@react-native-firebase/app";
+import firestore from "@react-native-firebase/firestore";
+import "@react-native-firebase/auth";
 
 const data = {
-	name: 'Los Angeles',
-	state: 'CA',
-	country: 'USA'
+  name: "Los Angeles",
+  state: "CA",
+  country: "USA",
 };
 
-export let familyCode : string;
+export let familyCode: string;
+export let userId: string;
 
 //   var database = firebase.database();
-export const  getData = async() => {
-	const usersCollection = firestore().collection('family');
-	const snap = await firestore()
-	.collection('family')
-	.get();
-	(await snap).docs.map((doc) => {
-		console.log(doc.data());
-	})
-}
+export const getData = async () => {
+  const usersCollection = firestore().collection("family");
+  const snap = await firestore().collection("family").get();
+  (await snap).docs.map((doc) => {
+    console.log(doc.data());
+  });
+};
 
-export const getCollectionGroup = async(uid : string) => {
-	const snapshot = firestore().collectionGroup("list").get();
-	// .where('uid' , '==' , uid).get();
-	const user = (await snapshot).docs[0].data();
-	console.log((await snapshot).docs[0].data());
-	familyCode = user.family_id;
-	// const user = (await snapshot).docs[0].data();
-	// console.log("get",user);
-}
+export const getCollectionGroup = async (uid: string) => {
+  const snapshot = firestore().collectionGroup("list").get();
+  // .where('uid' , '==' , uid).get();
+  const user = (await snapshot).docs[0].data();
+  console.log((await snapshot).docs[0].data());
+  familyCode = user.family_id;
+  userId = user.id;
+  // const user = (await snapshot).docs[0].data();
+  // console.log("get",user);
+};
